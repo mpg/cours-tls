221df6e98268486954ba656dee2618f096b492b7
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2016-03-12 16:42:43 +0100

    Add prep.sh for grading projects

---
+++ prep.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# Get projects ready for being graded
+# Assumes all projects are at the root
+
+set -eu
+
+rm -rf rendu
+mkdir rendu
+ln -s ../mbedtls rendu/mbedtls
+
+mpgdiff() {
+    diff $1 ../../project-1/$1 > ${1}.mpg.diff
+    diff -w $1 ../../project-1/$1 > ${1}.mpg.diff-w
+}
+
+for fn in *.zip *.tgz *.tar*; do
+    name="${fn%%.*}"
+    printf "${name}... "
+
+    if [ -d "rendu/$name" ]; then false; fi
+    cp -r project-1 "rendu/$name"
+
+    cd "rendu/$name"
+
+    if echo "$fn" | grep '\.zip$' > /dev/null; then
+        unzip -o ../../$fn >/dev/null
+    else
+        tar xf ../../$fn
+    fi
+
+    if [ -e mitm_proxy.c ]; then :; else
+        touch BAD-LAYOUT-DIR
+        if [ -d project-1 ]; then
+            cp -r project-1/* .
+        fi
+    fi
+
+    if ls | grep '\.mpg\.diff'; then false; fi
+
+    mpgdiff mitm_proxy.c || true
+    mpgdiff mitm_proxy.c -b || true
+
+    if mpgdiff vulnerable_client.c; then
+        rm vulnerable_client.c.mpg.diff*
+    else
+        touch BAD-CLIENT-MODIFIED
+    fi
+
+    if mpgdiff Makefile; then
+        rm Makefile.mpg.diff*
+    else
+        touch BAD-MAKEFILE-MODIFIED
+    fi
+
+    cd ../..
+    echo "done"
+done
